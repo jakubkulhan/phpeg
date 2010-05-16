c777c23fe609251cf7cee991a0df68cf6a4b4ba4
Jakub Kulhan <jakub.kulhan@gmail.com>
2010-05-16 13:30:59 +0200

    Minor speed up, reporting expected symbols

--- lib/phpgen.php
+++ lib/phpgen.php
@@ -28,33 +28,33 @@ function {$this->phpeg->getName()}(\$s, array \$opts = array()) {\n
     if (!class_exists('__{$this->phpeg->getName()}')) {
 
         class __{$this->phpeg->getName()} {
-            private \$_s;
-            private \$_p;
-            private \$_maxp;
-            private \$_memo = array();
+            var \$_s;
+            var \$_p;
+            var \$_maxp;
+            var \$_expected;
+            var \$_memo;
 
-            public function __construct(\$_s, array \$opts = array())
+            function __construct(\$_s, array \$opts = array())
             {
                 foreach (\$opts as \$n => \$v) { \$this->\$n = \$v; }
                 \$this->_s = \$_s;
                 \$this->_p = \$this->_maxp = 0;
+                \$this->_expected = \$this->_memo = array();
             }
 
-            public function _p()
+            function _failed(\$s)
             {
-                if (func_num_args() < 1) {
-                    return \$this->_p;
+                if (\$this->_p > \$this->_maxp) {
+                    \$this->_maxp = \$this->_p;
+                    \$this->_expected = array();
                 }
 
-                \$this->_p = func_get_arg(0);
-
-                \$this->_maxp = max(\$this->_maxp, \$this->_p);
-                if (func_num_args() === 2 && func_get_arg(1)) {
-                    \$this->_maxp = \$this->_p;
+                if (!in_array(\$s, \$this->_expected)) {
+                    \$this->_expected[] = \$s;
                 }
             }
 
-            public function _getmemo(\$r, \$p)
+            function _getmemo(\$r, \$p)
             {
                 \$k = ((string) \$r) . ':' . ((string) \$p);
                 if (isset(\$this->_memo[\$k])) {
@@ -63,14 +63,9 @@ function {$this->phpeg->getName()}(\$s, array \$opts = array()) {\n
                 return NULL;
             }
 
-            public function _setmemo(\$r, \$p, \$v)
+            function _setmemo(\$r, \$p, \$v)
             {
                 \$this->_memo[((string) \$r) . ':' . ((string) \$p)] = \$v;
-            }
-
-            public function _maxp()
-            {
-                return \$this->_maxp;
             }
 
             $inner
@@ -78,8 +73,8 @@ function {$this->phpeg->getName()}(\$s, array \$opts = array()) {\n
     }
 
     \$i = new __{$this->phpeg->getName()}(\$s, \$opts);
-    \$ret = \$i->{$this->phpeg->getStartingRuleName()}();
-    return array_merge(\$ret, array(\$i->_maxp()));
+    \$ret = \$i->_parse_{$this->phpeg->getStartingRuleName()}();
+    return array_merge(\$ret, array(\$i->_maxp, \$i->_expected));
 }
 
 E;
@@ -137,20 +132,20 @@ E;
 
         if (isset($this->dlr[$name])) {
             return <<<E
-public function $name() {
-    \$m = \$this->_getmemo('$name', \$oldp = \$this->_p());
+function _parse_$name() {
+    \$m = \$this->_getmemo('$name', \$oldp = \$this->_p);
     if (\$m === NULL) {
         \$this->_setmemo('$name', \$oldp, 0);
-        \$newm = array_merge(\$this->_{$name}_(), array(\$newp = \$this->_p()));
+        \$newm = array_merge(\$this->_{$name}_(), array(\$newp = \$this->_p));
         \$prevm = \$this->_getmemo('$name', \$oldp);
         \$this->_setmemo('$name', \$oldp, \$newm);
 
         if (\$prevm === 1) {
             \$stop = \$newp;
             for (;;) {
-                \$this->_p(\$oldp);
+                \$this->_p = \$oldp;
                 \$ans = \$this->_{$name}_();
-                \$newp = \$this->_p();
+                \$newp = \$this->_p;
                 if (!\$ans[0] || \$newp <= \$stop) { break; }
                 \$newm = array_merge(\$ans, array(\$newp));
                 \$this->_setmemo('$name', \$oldp, \$newm);
@@ -165,11 +160,11 @@ public function $name() {
         return array(FALSE, NULL);
     }
 
-    \$this->_p(\$m[2]);
+    \$this->_p = \$m[2];
     return array(\$m[0], \$m[1]);
 }
 
-public function _{$name}_() {        
+function _{$name}_() {        
     $g
     return \$_$i;
 }
@@ -179,14 +174,14 @@ E;
         } else {
 
             return <<<E
-public function $name() {
-    \$_m = \$this->_getmemo('$name', \$_oldp = \$this->_p());
+function _parse_$name() {
+    \$_m = \$this->_getmemo('$name', \$_oldp = \$this->_p);
     if (\$_m === NULL) {
         $g
-        $safety\$this->_setmemo('$name', \$_oldp, \$_m = array_merge(\$_$i, array(\$this->_p())));
+        $safety\$this->_setmemo('$name', \$_oldp, \$_m = array_merge(\$_$i, array(\$this->_p)));
         return \$_$i;
     }
-    \$this->_p(\$_m[2]);
+    \$this->_p = \$_m[2];
     return array(\$_m[0], \$_m[1]);
 }
 
@@ -203,13 +198,13 @@ E;
         $myi = $i++;
         $ret = "";
         $ret .= "\$_$myi = array(FALSE, NULL);\n" .
-                "\$_pos$myi = \$this->_p();\n" .
+                "\$_pos$myi = \$this->_p;\n" .
                 "do {\n";
 
         foreach ($args as $tree) {
             $ret .= $this->g($i, $tree);
             $ret .= "if (!\$_{$i}[0]) {\n" .
-                        "\$this->_p(\$_pos$myi);\n" .
+                        "\$this->_p = \$_pos$myi;\n" .
                     "} else {\n" .
                         "\$_$myi = \$_$i;\n" .
                         "break;\n" .
@@ -293,29 +288,29 @@ E;
     /** @return string */
     private function gand($i, $tree)
     {
-        return "\$_pos$i = \$this->_p();\n" .
+        return "\$_pos$i = \$this->_p;\n" .
                $this->g($i + 1, $tree) .
                "\$_$i = array(\$_" . ($i + 1) . "[0], NULL);\n" .
-               "\$this->_p(\$_pos$i, TRUE);\n";
+               "\$this->_p = \$_pos$i;\n";
     }
 
     /** @return string */
     private function gnot($i, $tree)
     {
-        return "\$_pos$i = \$this->_p();\n" .
+        return "\$_pos$i = \$this->_p;\n" .
                $this->g($i + 1, $tree) .
                "\$_$i = array(!\$_" . ($i + 1) . "[0], NULL);\n" .
-               "\$this->_p(\$_pos$i, TRUE);\n";
+               "\$this->_p = \$_pos$i;\n";
     }
 
     /** @return string */
     private function gopt($i, $tree)
     {
         return "\$_$i = array(TRUE, NULL);\n" .
-               "\$_pos$i = \$this->_p();\n" .
+               "\$_pos$i = \$this->_p;\n" .
                $this->g($i + 1, $tree) .
                "if (!\$_" . ($i + 1) . "[0]) {\n" .
-                   "\$this->_p(\$_pos$i, TRUE);\n" .
+                   "\$this->_p = \$_pos$i;\n" .
                "} else {\n" .
                    "\$_$i = \$_" . ($i + 1) . ";\n" .
                "}\n";
@@ -326,12 +321,11 @@ E;
     {
         return "\$_$i = array(TRUE, array());\n" .
                "do {\n" .
-                   "\$_pos$i = \$this->_p();\n" .
+                   "\$_pos$i = \$this->_p;\n" .
                    $this->g($i + 1, $tree) .
-                   "if (!\$_" . ($i + 1) . "[0]) { \$this->_p(\$_pos$i, TRUE); }\n" .
+                   "if (!\$_" . ($i + 1) . "[0]) { \$this->_p = \$_pos$i; }\n" .
                    "else { \$_{$i}[1][] = \$_" . ($i + 1) . "[1]; }\n" .
-               "} while (\$_" . ($i + 1) . "[0]);\n"//;
-               .
+               "} while (\$_" . ($i + 1) . "[0]);\n" .
                ($this->isSimple($tree) ? "\$_{$i}[1] = implode('', (array) \$_{$i}[1]);\n" : '');
     }
 
@@ -343,43 +337,33 @@ E;
                "if (\$_" . ($i + 1) . "[0]) { \$_{$i}[1] = (array) \$_{$i}[1]; \$_{$i}[1][] = \$_" . ($i + 1) . "[1]; }\n" .
                "\$_{$i}[0] = \$_" . ($i + 1) . "[0];\n" .
                "while (\$_" . ($i + 1) . "[0]) {\n" .
-                   "\$_pos$i = \$this->_p();\n" .
+                   "\$_pos$i = \$this->_p;\n" .
                    $this->g($i + 1, $tree) .
                    "\$_{$i}[1] = (array) \$_{$i}[1];\n" .
-                   "if (!\$_" . ($i + 1) . "[0]) { \$this->_p(\$_pos$i); }\n" .
+                   "if (!\$_" . ($i + 1) . "[0]) { \$this->_p = \$_pos$i; }\n" .
                    "else { \$_{$i}[1][] = \$_" . ($i + 1) . "[1]; };\n" .
-               "}\n"//;
-               .
+               "}\n" .
                ($this->isSimple($tree) ? "\$_{$i}[1] = implode('', (array) \$_{$i}[1]);\n" : '');
     }
 
     /** @return string */
     private function gapp($i, $name)
     {
-        return "\$_$i = \$this->$name();\n";
+        return "\$_$i = \$this->_parse_$name();\n";
     }
 
 
     /** @return string */
     private function glit($i, $s)
     {
-        $encapsed_s = '"';
-
-        foreach (str_split($s) as $c) {
-            if ($c === "\\") { $encapsed_s .= "\\\\"; }
-            else if (ctype_print($c) || $c === ' ') { $encapsed_s .= $c; }
-            else {
-                $hex = dechex(ord($c));
-                $encapsed_s .= '\x' . (strlen($hex) < 2 ? str_pad($hex, 2, '0', STR_PAD_LEFT) : $hex);
-            }
-        }
-
-        $encapsed_s .= '"';
+        $encapsed_s = '"' . $this->formatForString($s) . '"';
 
         return "\$_$i = array(FALSE, NULL);\n" .
-               "if ((\$_{$i}_ = substr(\$this->_s, \$this->_p(), " . strlen($s) . ")) === " . $encapsed_s . ") {\n" .
+               "if ((\$_{$i}_ = substr(\$this->_s, \$this->_p, " . strlen($s) . ")) === " . $encapsed_s . ") {\n" .
                    "\$_$i = array(TRUE, \$_{$i}_);\n" .
-                   "\$this->_p(\$this->_p() + " . strlen($s) . ");\n" .
+                   "\$this->_p += " . strlen($s) . ";\n" .
+               "} else {\n" .
+                   "\$this->_failed(" . var_export($encapsed_s, TRUE) . ");\n" .
                "}\n";
     }
 
@@ -390,30 +374,59 @@ E;
         $i = array_shift($args);
 
         $or = array();
+        $str = "";
 
         foreach ($args as $r) {
             if (is_array($r)) {
                 $or[] = "(" . $r[0] . " <= \$_{$i}_ && \$_{$i}_ <= " . $r[1] . ")";
+                $str .= chr($r[0]) . "-" . chr($r[1]);
             } else {
                 $or[] = "(\$_{$i}_ === $r)";
+                $str .= chr($r);
             }
         }
 
+        $str = $this->formatForString("[" . $str . "]");
+
         return "\$_$i = array(FALSE, NULL);\n" .
-               "\$_{$i}_ = ord(substr(\$this->_s, \$this->_p(), 1));\n" .
-               "if (isset(\$this->_s[\$this->_p()]) && (" . implode(" || ", $or) . ")) {\n" .
-                   "\$_$i = array(TRUE, substr(\$this->_s, \$this->_p(), 1));\n" .
-                   "\$this->_p(\$this->_p() + 1);\n" .
-               "}\n"; 
+               "\$_{$i}_ = ord(substr(\$this->_s, \$this->_p, 1));\n" .
+               "if (isset(\$this->_s[\$this->_p]) && (" . implode(" || ", $or) . ")) {\n" .
+                   "\$_$i = array(TRUE, substr(\$this->_s, \$this->_p, 1));\n" .
+                   "\$this->_p += 1;\n" .
+               "} else {\n" .
+                   "\$this->_failed(" . var_export($str, TRUE) . ");\n" .
+               "}\n";
+    }
+
+    /** @return string */
+    private function formatForString($s)
+    {
+        $formatted = "";
+
+        foreach (str_split($s) as $c) {
+            if ($c === "\\") { $formatted .= "\\\\"; }
+            else if (ctype_print($c) || $c === ' ') { $formatted .= $c; }
+            else if ($c === "\t") { $formatted .= "\\t"; }
+            else if ($c === "\n") { $formatted .= "\\n"; }
+            else if ($c === "\r") { $formatted .= "\\r"; }
+            else {
+                $hex = dechex(ord($c));
+                $formatted .= '\x' . (strlen($hex) < 2 ? str_pad($hex, 2, '0', STR_PAD_LEFT) : $hex);
+            }
+        }
+
+        return $formatted;
     }
 
     /** @return string */
     private function gany($i)
     {
-        return "\$_$i = array(isset(\$this->_s[\$this->_p()]), NULL);\n" .
+        return "\$_$i = array(isset(\$this->_s[\$this->_p]), NULL);\n" .
                "if (\$_{$i}[0]) {\n" . 
-                   "\$_{$i}[1] = \$this->_s[\$this->_p()];\n" .
-                   "\$this->_p(\$this->_p() + 1);\n" .
+                   "\$_{$i}[1] = \$this->_s[\$this->_p];\n" .
+                   "\$this->_p += 1;\n" .
+               "} else {\n" .
+                   "\$this->_failed('any character');\n" .
                "}\n";
     }
 
@@ -435,7 +448,7 @@ E;
         }
 
         $methodname = "_" . count($this->methods);
-        $this->methods[] = "private function " . $methodname . "(" . implode (", ", $params) . ") {\n" .
+        $this->methods[] = "function " . $methodname . "(" . implode (", ", $params) . ") {\n" .
                            $code .
                            "\n}\n";
 
